5e16f2dce4729c9a23681b12f62b9bbb49bbf1d9
AdityaByte <adityapawar4914@gmail.com>
2026-08-25 21:53:53 +0530

    fixes #9: Implemented embedding manager for data ingestion pipeline

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,10 +11,26 @@
 #     uvicorn.run(app, host="localhost", port=4000)
 
 from app.core.document_loader import JSONDocumentLoader
+from app.config.logger import setup_logging
+from app.core.embedding_manager import EmbeddingManager
+
+setup_logging()
 
 def main():
     doc_loader = JSONDocumentLoader("data/food_standards_dataset.jsonl")
     result = doc_loader.load()
-    print(result[0])
 
-main()
+    if not result:
+        raise ValueError("Failed to create the document struture")
+
+    # Once the documents has been loaded successfully.
+    # We need to create a EmbeddingManager Object.
+    embedding_manager = EmbeddingManager()
+    page_contents = [doc.page_content for doc in result]
+    embeddings = embedding_manager.generate_embeddings(texts=page_contents)
+    print(type(embeddings))
+    print(embeddings[0])
+
+
+if __name__ == "__main__":
+    main()
