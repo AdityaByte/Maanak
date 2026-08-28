e6bb87dc1ab5ac0149ba39fb6fd820da7a525d97
Tanisha Dhosele <tanishadhosele@gmail.com>
2026-08-28 18:03:14 +0530

    Created Overview tab as resusable component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import HeroSection from './components/HeroSection';
+import Overview from './components/Overview/Overview';
 
 export default function DashboardPage() {
   return (
     <div className="w-full">
       <HeroSection />
+
+      <Overview/>
        <div className="h-[2000px]" /> {/* remove after testing */}
     </div>
   );
